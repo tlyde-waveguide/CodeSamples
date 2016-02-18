ad0e030ee07912b1ef0a79cb076027a9b0c0aa91
tlyde-waveguide <tlyde@waveguide.com>
2016-02-18 11:09:17 -0500

    First Entry for CodeShort1

---
+++ CodeShort1_TL/CodeShort1_TL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeShort1_TL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
